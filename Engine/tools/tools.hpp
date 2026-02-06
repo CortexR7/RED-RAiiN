b637b7cab3d618adf7f49add1f2a475b323d47e0
CortexR7 <128260156+YaminAhmed123@users.noreply.github.com>
2026-02-07 00:35:11 +0100

    Remove GenericArray class from tools.hpp

--- Engine/tools/tools.hpp
+++ Engine/tools/tools.hpp
@@ -1,5 +1,6 @@
 #ifndef TOOLS_H
 #define TOOLS_H
+#include <iostream>
 
 /* 
 * Copyright (c) 2025 CortexR7
@@ -9,71 +10,11 @@
 */
 
 namespace tools{
-    // Generics
-
-    // P = preAllocation Size
-    // S = size
-    // T = type
-    // K = increment size if currentCapacity is reached
-    template<int K,int P,int S,typename T>
-    class GenericArray
-    {
-        private:
-        int size = 0;
-        int currentCapacity;
-        public:
-        T* array;
-
-        GenericArray() : currentCapacity(S+P)
-        {
-            size = S;
-            array = new T[size+P];    // WARNING: allocations must be freed later
-        }
-
-        ~GenericArray()
-        {
-            delete[] array;         // memory getting freed
-        }
-
-        T* getPointer()
-        {
-            return array;
-        }
-
-    
-        int getSize()
-        {
-            return size;
-        }
-
-        void addElement(T element)
-        {
-            if(size < currentCapacity){
-                ++size;
-                array[size-1] = element;
-            }   else{
-                T* newArray = new T[currentCapacity + K];
-                for(int i = 0; i < size; ++i){
-                    newArray[i] = array[i];
-                }
-                newArray[size] = element;
-                ++size;
-                currentCapacity += K;
-                delete[] array;
-                array = newArray;
-            }
-        }
-    };
-
-
-
-    
     #ifdef LOGGER_ON
     #define LOG(msg) std::cout << "\033[34m" << "[LOGGER]: " << msg << "\033[0m" << std::endl;
     #else    
     #define LOG(msg)
     #endif
-    
 }
 
 #endif
