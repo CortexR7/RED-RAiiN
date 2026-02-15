eccc53bca13c3e3985d9264d420b9a5c90ac58be
cex7 <yaminahmed821@gmail.com>
2026-02-15 16:24:50 +0100

    adding a port from glsl to slang (if you wonder why the instnace was touched here. Its because slnag officially supports SPIR-V 1.3. lower version support is experimental and thats why I decided to increase instance version by minor 1)

--- Engine/engine/vulkanaid/src/createvkinstance.cpp
+++ Engine/engine/vulkanaid/src/createvkinstance.cpp
@@ -33,7 +33,7 @@ void createVkInstance(VkInstance& instance)
     appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
     appInfo.pEngineName = "RED-RAiiN ENGINE";
     appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-    appInfo.apiVersion = VK_MAKE_API_VERSION(0, 1, 0, 0);
+    appInfo.apiVersion = VK_MAKE_API_VERSION(0, 1, 1, 0);
 
     VkInstanceCreateInfo createInfo{};
     createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
