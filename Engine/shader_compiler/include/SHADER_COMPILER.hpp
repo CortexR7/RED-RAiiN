1305f1440803401df1169eb07008d0ad815043ed
cex7 <yaminahmed821@gmail.com>
2026-03-05 14:20:04 +0100

    working on shader compiler saving wor for now.

--- Engine/shader_compiler/include/SHADER_COMPILER.hpp
+++ Engine/shader_compiler/include/SHADER_COMPILER.hpp
@@ -2,11 +2,16 @@
 #define SHADER_COMPILER
 
 #include <vulkan/vulkan.h>
+#include <slang/slang.h>
+#include <slang/slang-com-ptr.h>
+#include <slang/slang-com-helper.h>
 
 class SLANG2SPIRV{
     private:
+    Slang::ComPtr<slang::IGlobalSession> globalSession;
 
     public:
+    void createSoloSession();
 
     SLANG2SPIRV();
     ~SLANG2SPIRV();
--- Engine/shader_compiler/src/SHADER_COMPILER.cpp
+++ Engine/shader_compiler/src/SHADER_COMPILER.cpp
@@ -1,5 +1,27 @@
 #include <SHADER_COMPILER.hpp>
 
 
-SLANG2SPIRV::SLANG2SPIRV(){}
+SLANG2SPIRV::SLANG2SPIRV()
+{
+    createGlobalSession(this->globalSession.writeRef());
+}
 SLANG2SPIRV::~SLANG2SPIRV(){}
+
+
+void SLANG2SPIRV::createSoloSession()
+{
+    slang::SessionDesc sessionDesc = {};
+
+    slang::TargetDesc targetDesc = {};
+    targetDesc.format = SLANG_SPIRV;
+    targetDesc.profile = this->globalSession->findProfile("spirv_1_3");
+
+    sessionDesc.targets = &targetDesc;
+    sessionDesc.targetCount = 1;
+
+    // skip macros for now
+    sessionDesc.preprocessorMacros = nullptr;
+    sessionDesc.preprocessorMacroCount = 0;
+
+    
+}
