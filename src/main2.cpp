24fb43fb73c675cffb2c2e60395ad796e9167a49
CortexR7 <yaminahmed821@gmail.com>
2026-06-15 19:44:23 +0200

    remooving tools.hpp and using ETLs debug logger instead

--- src/main2.cpp
+++ src/main2.cpp
@@ -1,19 +1,20 @@
 #define LOGGER_ON
 #include <render_engine.hpp>
 #include <RENDER_ENGINE2.hpp>
-#include <tools.hpp>
 #include <iostream>
 #include <SHADER_COMPILER.hpp>
+
+#define DEBUG_ON
 #include <ETL.hpp>
 
 
 int main(){
-    LOG("Starting RED-RAiiN Engine...");
+    DEBUG_LOG("Starting RED-RAiiN Engine...");
     
     RENDER_ENGINE2 RENDER_ENGINE;
     RENDER_ENGINE.RUN_ENGINE();
     
-    LOG("Exit Game Engine ...");
+    DEBUG_LOG("Exit Game Engine ...");
  
     return 0;
 }
