d7f91e3547849d1c5afbc8355988d704f8714199
CortexR7 <yaminahmed821@gmail.com>
2026-06-15 19:29:57 +0200

    adding a PIPLINE class to represent the next thing I will work on. In this case the GPU pipeline in Vulkan

---
+++ Engine/render_engine/include/VULKAN_PIPELINE.hpp
@@ -0,0 +1,16 @@
+#ifndef VULKAN_PIPELINE_HPP
+#define VULKAN_PIPELINE_HPP
+
+/*
+*  Copyright (c) 2026 CortexR7
+*
+*  This header defines the GPU Pipeline class which will
+*  handle the creation of the Pipeline and hold the ressources relative too it such as the shaders.
+*/
+
+class VULKAN_PIPELINE{
+    private:
+    public:
+};
+
+#endif
