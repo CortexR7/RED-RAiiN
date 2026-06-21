4ec7f295d42468bd266f1f944500056991fd95ea
CortexR7 <yaminahmed821@gmail.com>
2026-06-21 16:42:04 +0200

    adding swapchain images and image views

--- Engine/render_engine/src/VULKAN_SWAPCHAIN.cpp
+++ Engine/render_engine/src/VULKAN_SWAPCHAIN.cpp
@@ -145,9 +145,44 @@ void VULKAN_SWAPCHAIN::INIT(WINDOW WIN, VULKAN_PHYSICAL_DEVICE PH_DEVICE, VULKAN
     {
         throw std::runtime_error("failed to create a swapchain !");
     }
+
+
+    uint32_t imageCounter;
+    vkGetSwapchainImagesKHR(LG_DEVICE.GET_HANDLE_TO_VK_LOGICAL_DEVICE(), this->SW_CHAIN, &imageCounter, nullptr);
+    this->SW_CHAIN_IMAGES.resize(imageCounter);
+    vkGetSwapchainImagesKHR(LG_DEVICE.GET_HANDLE_TO_VK_LOGICAL_DEVICE(), this->SW_CHAIN, &imageCounter, this->SW_CHAIN_IMAGES.data());
+
+
+    this->SW_CHAIN_IMAGE_VIEWS.resize(this->SW_CHAIN_IMAGES.size());
+    for(size_t i = 0; i < this->SW_CHAIN_IMAGES.size(); i++)
+    {
+        VkImageViewCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+        createInfo.image = this->SW_CHAIN_IMAGES[i];
+        createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+        createInfo.format = this->SW_CHAIN_IMAGE_FORMAT;
+        createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+        createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+        createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+        createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+        createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+        createInfo.subresourceRange.baseMipLevel = 0;
+        createInfo.subresourceRange.levelCount = 1;
+        createInfo.subresourceRange.baseArrayLayer = 0;
+        createInfo.subresourceRange.layerCount = 1;
+        
+        if(vkCreateImageView(this->LG_DEVICE.GET_HANDLE_TO_VK_LOGICAL_DEVICE(), &createInfo, nullptr, &(this->SW_CHAIN_IMAGE_VIEWS[i])) != VK_SUCCESS)
+        {
+            throw std::runtime_error("failed to create image views!");
+        }
+    }
 }
 
 void VULKAN_SWAPCHAIN::FREE()
 {
+    for(auto& imageView : this->SW_CHAIN_IMAGE_VIEWS)
+    {
+        vkDestroyImageView(this->LG_DEVICE.GET_HANDLE_TO_VK_LOGICAL_DEVICE(), imageView, nullptr);
+    }
     vkDestroySwapchainKHR(this->LG_DEVICE.GET_HANDLE_TO_VK_LOGICAL_DEVICE(), this->SW_CHAIN, nullptr);
 }
