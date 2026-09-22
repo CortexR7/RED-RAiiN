58fcffd67ebe142ef99121e48748a7098774e8ac
Cortex R7 <CortexR7@outlook.de>
2026-09-23 01:59:38 +0200

    adding Comments to the VULKAN_DESC_SET.hpp file

--- Engine/render_engine/include/VULKAN_DESC_SET.hpp
+++ Engine/render_engine/include/VULKAN_DESC_SET.hpp
@@ -1,6 +1,18 @@
 #ifndef VULKAN_DESC_SET_HPP
 #define VULKAN_DESC_SET_HPP
 
+/*
+*  Copyright (c) 2026 CortexR7
+*
+*  This header defines the VK DESC_SET.
+*  It represents an object that does follwing things:
+*
+*  - It stores descriptors sets of "DESC_COUNT"
+*  - It stores the descriptor pool from where th sets get allocated
+*  - It stores exacltly ONE descriptor set layout (all other x amount of descripto sets are based on that layout.) x = DESC_SET
+*
+*/
+
 #include <cstdint>
 #include <vulkan/vulkan.h>
 #include <VULKAN_LOGICAL_DEVICE.hpp>
@@ -19,8 +31,8 @@ class VULKAN_DESC_SET{
 
     public:
         void INIT(VULKAN_LOGICAL_DEVICE LGD, uint32_t DESC_COUNT);
-        void UPDATE_SETS(RE2_UBO_OBJ& UBO);
-        void FREE();
-};
+        void UPDATE_SETS(RE2_UBO_OBJ& UBO);                         // WARNING: Its is probably good to mention that the UBO passed here MUST
+        void FREE();                                                // have a UBO_COUNT that matches the DESC_COUNT. Else a segfault will occur due to
+};                                                                  // an out of bounds exception if DESC_COUNT > UBO_COUNT.
 
 #endif
